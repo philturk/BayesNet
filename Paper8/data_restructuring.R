4331b3aa26fc8ac9db54f6bf82787e49a913cf13
lemiregm <gabrielle.lemire@outlook.com>
2021-05-12 11:28:57 -0600

    added melt all props function

--- Paper8/data_restructuring.R
+++ Paper8/data_restructuring.R
@@ -8,47 +8,15 @@ library(reshape2)
 sim_dist_100 = read.csv("C://Code//msu//gra//BayesNet//Paper8//sim_dists.csv")
 true_dist_1 = read.csv("C://Code//msu//gra//BayesNet//Paper8//true_dist.csv")
 
-# --- first proportion. 
-# After this will be in a loop where rows 1:23 are .95, 24:46 are .9
-# see below for attempted loop
-# create df in format: 
-# one column for component size count, 
-# one column for which sim it came from
+# keep until discrepancy of 10 nodes in loop is solved
+# delete after
 df = as.data.frame(sim_dist_100[1:23,])
 df = melt(df, id.vars="X")
 head(df)
 
-# this is a loop for each of the proportions... 
-# create df with one column per proportion (includes 100 simulations)
-for (i in c(1:16)){
-  df = as.data.frame(sim_dist_100[(i+((i-1)*22)):(i*23),])
-  df$X = c(1:23)
-  df = melt(df, id.vars="X")
-  df_all_sim[as.character(samp_prop[i])] = df$value
-}
+# --------------- FUNCTIONS
 
-# --- split one sim
-
-
-# # ------------create df with one row = 1 component
-# # create a dataframe for one simulation (ex: X1)
-# # will need to nest this in another loop to do the same for all of X1, X2... X100
-# # delete after
-# row_count = c()
-# data = data.frame(comp_size=integer())
-# for (i in 1:23){
-#   rows = df$value[i] #how many rows to create
-#   input = df$X[i] #which value to input into row
-#   start = sum(row_count)
-#   data[(start+1):(start+rows),1] = input
-#   row_count = append(row_count, rows)
-# }
-# table(data$comp_size)
-# ------------
-
-
-
-# one sim from a df with one proportion
+# --------------SPLIT ONE SIMULATION
 split_one_sim = function(beg = 1, end = 23, dataframe = df){
   # beg is starting row in df to split
   # end is last row in df to split
@@ -67,36 +35,15 @@ split_one_sim = function(beg = 1, end = 23, dataframe = df){
       row_count = append(row_count, rows)
     }
   }
-
+  
   # add a column with which sim it's from
   var = paste0("sim_", (beg-1)/23+1)
   data[,1] = var
   
   return(data)
-  }
+}
 
-# # ---checks
-# # AHHH why aren't these lining up?! -- stop to eat
-# # create and combine all the sims from one proportion
-# test_1 = split_one_sim(beg=1, end=23)
-# df$value[1:23]
-# table(test_1$comp_size)
-# 
-# sum(df$value[1:23]) #798 components
-# length(test_1$comp_size)#798 components YES
-# 
-# crossprod(df$X[1:23],df$value[1:23]) #1195 total nodes
-# sum(test_1$comp_size) #1195 total nodes
-# 
-# test2 = split_one_sim(beg=24, end=46)
-# sum(df$value[24:46]) #802
-# table(test2$comp_size)
-# df$value[24:46] 
-# 
-# test3 = split_one_sim(beg=47, end=69)
-# table(test3$comp_size)
-# df$value[47:69]
-
+# --------------- COMBINE ALL SIMULATIONS
 combine_all_sim = function(){
   beg_100 = seq(from = 1, to = 2300, by = 23)
   end_100 = seq(from = 23, to = 2300, by = 23)
@@ -110,15 +57,60 @@ combine_all_sim = function(){
   return(combined)
 }
 
-data = combine_all_sim() #length of data is 79938
-crossprod(df$X,df$value) #119500 total nodes
-sum(data$comp_size) #119500
+# --------------- MELT ALL PROPORTIONS
+melt_all_props = function(){
+  beg_prop = seq(from = 1, to = 346, by = 23)
+  end_prop = seq(from = 23, to = 368, by = 23)
+  samp_prop = seq(from = .95, to = 0.2, by = -0.05)
+  # start_row = seq(from = 0, to = 34500, by = 2300) #maybe don't need if I use rbind
+  df = data.frame()
+  df_all_sim = data.frame()
+  for (i in 1:length(beg_prop)){
+    beg = beg_prop[i]
+    end = end_prop[i]
+    df = as.data.frame(sim_dist_100[beg:end,])
+    df$X = c(1:23) #leave as 1 to 23 because all props should have this component count
+    df = melt(df, id.vars="X")
+    df_all_sim = rbind(df_all_sim, df)
+  }
+  return(df_all_sim)
+}
+
+
+# last step - nest them all in each other
+df_all_sim = melt_all_props()
+
+
+
+
+# --- check
+# WHY ISN'T THIS ADDING UP?
+# check the sim is sampling .95 nodes versus components
+crossprod(true_dist_1$X,true_dist_1$x)*100 #125800
+crossprod(df_all_sim$X[1:2300],df_all_sim$value[1:2300]) #119500
+125800*.95 #119510 <-- shouldn't this be the crossproduct of above? off by 10
+
+crossprod(true_dist_1$X,true_dist_1$x)*100 #125800
+crossprod(df_all_sim$X[2301:4600],df_all_sim$value[2301:4600]) #113200
+125800*.9 #113220 <-- off by 10 again
+
+
+
+
+# can build in print-outs for checks if desired...
+# # ---checks for output of one loop of split_one_sim()
+# test_1 = split_one_sim(beg=1, end=23)
+# df$value[1:23]
+# table(test_1$comp_size)
 # 
+# sum(df$value[1:23]) #798 components
+# length(test_1$comp_size)#798 components YES
 # 
-# sum(df$value) #79868
-# table(data$comp_size[1:799])
-# table(data$comp_size[800:800+802])
-# beg_100
-# end_100
-# # 79938-sum(df$value) is 70
-# 
+# crossprod(df$X[1:23],df$value[1:23]) #1195 total nodes
+# sum(test_1$comp_size) #1195 total nodes
+
+# --- checks for output of combine_all_sim() for one proportion
+# data = combine_all_sim() #length of data is 79938
+# crossprod(df$X,df$value) #119500 total nodes
+# sum(data$comp_size) #119500
+
