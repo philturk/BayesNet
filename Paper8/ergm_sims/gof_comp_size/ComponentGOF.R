1d8462a6b6f2823fbf4a9c5b0cc7f79545ba4e8d
lemiregm <gabrielle.lemire@outlook.com>
2021-07-07 12:28:25 -0600

    add 1000 sim data and ergm files

--- Paper8/ergm_sims/gof_comp_size/ComponentGOF.R
+++ Paper8/ergm_sims/gof_comp_size/ComponentGOF.R
@@ -12,15 +12,17 @@ ComponentGOF<-function(ergm,nsim=100){
   #ergm: output from ergm()
   #nsim: number of networks to simulate (default of 100)
   
-  ncomp<-9
+  ncomp<-25
   #Store original network
   network<-ergm$network
   #Store component distribution of original network
-  true<-t(component.dist(network)$cdist)
+  obs<-t(component.dist(network)$cdist)
   #Count number of nodes
-  nodes<-length(true)
+  nodes<-length(obs)
+  #Combine component sizes past ncomp
+  obs[ncomp]<-sum(obs[ncomp:nodes])
   #Store component distribution of original network as wide data
-  wide<-as.data.frame(true)
+  wide<-as.data.frame(obs)
   #Simulate and store specified number of networks
   sims<-simulate(ergm,nsim)
   #Loop through all network simulations
@@ -28,18 +30,36 @@ ComponentGOF<-function(ergm,nsim=100){
     #Store component distribution for simulation i
     wide[i+1,]<-component.dist(sims[[i]])$cdist
     #Combine largest component size considered with all others
-    wide[i,ncomp]<-sum(wide[i,ncomp:nodes])
+    wide[i+1,ncomp]<-sum(wide[i,ncomp:nodes])
   }
-  #Exclude all component sizes largere than ncomp
+  #Exclude all component sizes larger than ncomp
   wide<-wide[,1:ncomp]
   #Store network number
   wide$network<-c(0:nsim)
+  #Store component sizes
+  sizes<-c(as.character(1:(ncomp-1)),paste(ncomp,"+",sep=""))
+  #Prepopulate vector of mean number of components by size
+  means<-matrix(0,ncomp,1)
+  #Loop through all component sizes considered
+  for(i in 1:ncomp){
+    means[i]<-mean(wide[2:nsim,i])
+  }
+  #Store component sizes
+  summary<-as.data.frame(sizes)
+  #Store true component distribution
+  summary$obs<-obs[1:ncomp]
+  #Store means for component distribution
+  summary$mean<-round(means,2)
+  #Print summary
+  print.data.frame(summary,row.names=FALSE)
   #Convert data to long format
   long<-gather(wide,size,number,V1:paste("V",ncomp,sep=""))
+  #Convert component size from character
+  long$size<-rep(1:ncomp,each=nsim+1)
   #Convert component size to factor
   long$size<-factor(long$size)
-  #Relable component sizes
-  levels(long$size)<-c("1","2","3","4","5","6","7","8","9+")
+  #Relabel component sizes
+  levels(long$size)<-sizes
   ggplot(long,aes(x=size,y=number))+
     geom_boxplot(data=long[long$network!=0,])+
     geom_point(data=long[long$network==0,])+
@@ -47,4 +67,5 @@ ComponentGOF<-function(ergm,nsim=100){
     labs(x="Component Size",y="Number of Components")
 }
 
-#ComponentGOF(ergm)
+#Test
+#ComponentGOF(ergm,10)
