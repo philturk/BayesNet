52164f42a47b8f31a1e036e0df26424c49d42932
lemiregm <gabrielle.lemire@outlook.com>
2021-09-01 14:15:22 -0600

    added incremental .5 to .2 by .05 with 100 subsamples at each rate

---
+++ Paper8/comp_dist/subsampling/subsampling.05increments.R
@@ -0,0 +1,66 @@
+set.seed(2021)
+library(sna)
+
+#Build complete genetic network
+getwd()
+source("comp_dist//Network.R") 
+
+# declare variables to go from 50 to 20 in increments of .05 with 100 subsamples at each level
+nsims = 100 #number of subsamples for each sampling proportion
+row_names = seq(from=.5, to=.2, by=-.05) #row names for array
+file_location = dirname(rstudioapi::getActiveDocumentContext()$path)
+sim_dist <- array(NA, dim = c(length(row_names), 23, nsims), 
+                  dimnames = list(row_names, 1:23, NULL)) #array to store sims
+
+
+for(i in 1:length(row_names)){
+  sample_rate = row_names[i]
+  for(j in 1:nsims){
+    #Randomly sample vertices to delete
+    deleted_vertices <- sample(1:1258, round(1258*(1-sample_rate)), replace = FALSE)
+    
+    #Build subnetwork without deleted vertices
+    sampledNetwork <- network(GeneticNetwork[-deleted_vertices, -deleted_vertices])
+    
+    # Compute cluster size distribution and store
+    sim_dist[i,,j] <- component.dist(sampledNetwork)$cdist[1:23]
+  }
+  print(paste0("completed sim ", as.character(i), " out of ", as.character(length(row_names))))
+}
+
+# change format, save
+new_list <- plyr::adply(sim_dist,1,unlist,.id = NULL)
+write.csv(new_list, paste0(file_location, "//sim_dists-incre-subsamp-comp_dist-100.csv"))
+
+# function for later
+melt_all_props7by100 = function(data){
+  library(reshape2)
+  # create one master df with the following columns:
+  # X - the size of the component
+  # variable - which sim it came from
+  # value - count of components
+  
+  #refers to beg and end row lines for each prop of sim_dist
+  beg_prop = seq(from = 1, to = 161, by = 23) 
+  end_prop = seq(from = 23, to = 161, by = 23)
+  # samp_prop <- rep(.8, 100) #does our function even use this?
+  df = data.frame()
+  df_all_sim = data.frame()
+  for (i in 1:length(beg_prop)){
+    beg = beg_prop[i]
+    end = end_prop[i]
+    df = as.data.frame(data[beg:end,])
+    df$X = c(1:23) #leave as 1 to 23 because all props should have this component count
+    df = melt(df, id.vars="X")
+    df_all_sim = rbind(df_all_sim, df)
+  }
+  return(df_all_sim)
+}
+
+
+# change format again, save
+df_all_sim = melt_all_props7by100(data = new_list)
+write.csv(df_all_sim, paste0(file_location, "//incremental//subsamples70by100.csv"), row.names = FALSE)
+
+# check/look at it
+length(df_all_sim$X) #16 100 = 100 (subsamples) *7 (props) *23 (comp sizes)
